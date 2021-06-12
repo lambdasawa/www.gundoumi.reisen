406625b8466023c981af38c28a319c689bfd7a11
Tsubasa Irisawa <lambdasawa@gmail.com>
2021-06-13 05:52:49 +0900

    Update navigation bar

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,28 +1,73 @@
 import AppBar from "@material-ui/core/AppBar";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
 import IconButton from "@material-ui/core/IconButton";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
+import MuiLink from "@material-ui/core/Link";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
-import MuiLink from "@material-ui/core/Link";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import React from "react";
 
+const drawerWidth = 120;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    drawer: {
+      [theme.breakpoints.up("sm")]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up("sm")]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+  })
+);
+
+const menu = [
+  { href: "/sei/tv", name: "MireiTV" },
+  { href: "/sei/illust", name: "イラスト" },
+  { href: "/sei/musics", name: "楽曲" },
+  { href: "/sei/buttons", name: "美玲ボタン" },
+  { href: "/sei/history", name: "年表" },
+  { href: "/sei/links", name: "リンク集" },
+  { href: "/sei/materials", name: "素材集" },
+];
+
 export default function Header() {
-  const router = useRouter();
+  const classes = useStyles();
 
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
   };
 
-  const handleClose = (url: string) => {
-    setAnchorEl(null);
-    router.push(url);
-  };
+  const drawer = (
+    <List>
+      {menu.map((m) => (
+        <ListItem button key={m.href}>
+          <Link href={m.href}>
+            <ListItemText primary={m.name} />
+          </Link>
+        </ListItem>
+      ))}
+    </List>
+  );
 
   return (
     <AppBar position="static" color="default">
@@ -31,33 +76,30 @@ export default function Header() {
           edge="start"
           color="inherit"
           aria-label="menu"
-          onClick={handleClick}
+          onClick={handleDrawerToggle}
         >
           <MenuIcon />
         </IconButton>
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={() => handleClose("/sei/history")}>年表</MenuItem>
-          <MenuItem onClick={() => handleClose("/sei/links")}>
-            リンク集
-          </MenuItem>
-          <MenuItem onClick={() => handleClose("/sei/illust")}>
-            イラスト
-          </MenuItem>
-          <MenuItem onClick={() => handleClose("/sei/musics")}>楽曲</MenuItem>
-          <MenuItem onClick={() => handleClose("/sei/buttons")}>
-            美玲ボタン
-          </MenuItem>
-          <MenuItem onClick={() => handleClose("/sei/tv")}>MireiTV</MenuItem>
-          <MenuItem onClick={() => handleClose("/sei/materials")}>
-            素材集
-          </MenuItem>
-        </Menu>
+
+        <nav className={classes.drawer} aria-label="mailbox folders">
+          <Hidden smUp implementation="css">
+            <Drawer
+              variant="temporary"
+              anchor="left"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              ModalProps={{
+                keepMounted: true,
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+
         <Typography variant="h6">
           <MuiLink className="top-logo">
             <Link href="/sei/">郡道美玲ファンサイト</Link>
