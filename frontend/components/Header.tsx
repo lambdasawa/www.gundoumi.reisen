62390d5d8f25eed220407ac4121b910eaecb47d0
Tsubasa Irisawa <lambdasawa@gmail.com>
2021-06-13 20:14:14 +0900

    Use Tabs and Tab

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,39 +1,18 @@
 import AppBar from "@material-ui/core/AppBar";
-import Drawer from "@material-ui/core/Drawer";
-import Hidden from "@material-ui/core/Hidden";
-import IconButton from "@material-ui/core/IconButton";
-import MuiLink from "@material-ui/core/Link";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import MenuIcon from "@material-ui/icons/Menu";
-import Link from "next/link";
-import React from "react";
-
-const drawerWidth = 120;
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: "flex",
     },
-    drawer: {
-      [theme.breakpoints.up("sm")]: {
-        width: drawerWidth,
-        flexShrink: 0,
-      },
-    },
-    appBar: {
-      [theme.breakpoints.up("sm")]: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-      },
-    },
-    drawerPaper: {
-      width: drawerWidth,
+    tab: {
+      textTransform: "none",
+      minWidth: 80,
     },
   })
 );
@@ -41,7 +20,7 @@ const useStyles = makeStyles((theme: Theme) =>
 const menu = [
   { href: "/sei/tv", name: "MireiTV" },
   // TODO: { href: "/sei/illust", name: "イラスト" },
-  { href: "/sei/musics", name: "楽曲" },
+  { href: "/sei/musics", name: "音楽" },
   { href: "/sei/buttons", name: "美玲ボタン" },
   { href: "/sei/history", name: "年表" },
   { href: "/sei/links", name: "リンク集" },
@@ -51,61 +30,34 @@ const menu = [
 export default function Header() {
   const classes = useStyles();
 
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const router = useRouter();
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+  const [value, setValue] = React.useState(0);
 
-  const drawer = (
-    <List>
-      {menu.map((m) => (
-        <ListItem button key={m.href}>
-          <Link href={m.href}>
-            <ListItemText primary={m.name} />
-          </Link>
-        </ListItem>
-      ))}
-    </List>
-  );
+  useEffect(() => {
+    setValue(
+      location ? menu.findIndex(({ href }) => href === location.pathname) : 0
+    );
+  });
 
   return (
     <AppBar position="static" color="default">
-      <Toolbar>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          onClick={handleDrawerToggle}
-        >
-          <MenuIcon />
-        </IconButton>
-
-        <nav className={classes.drawer} aria-label="mailbox folders">
-          <Hidden smUp implementation="css">
-            <Drawer
-              variant="temporary"
-              anchor="left"
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-              classes={{
-                paper: classes.drawerPaper,
-              }}
-              ModalProps={{
-                keepMounted: true,
-              }}
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
-        </nav>
-
-        <Typography variant="h6">
-          <MuiLink className="top-logo">
-            <Link href="/sei/">郡道美玲ファンサイト</Link>
-          </MuiLink>
-        </Typography>
-      </Toolbar>
+      <Tabs
+        variant="scrollable"
+        textColor="primary"
+        indicatorColor="secondary"
+        scrollButtons="on"
+        aria-label="scrollable force tabs"
+        value={value}
+        onChange={(_, newValue) => {
+          setValue(newValue);
+          router.push(menu[newValue].href);
+        }}
+      >
+        {menu.map((m) => (
+          <Tab key={m.href} label={m.name} className={classes.tab}></Tab>
+        ))}
+      </Tabs>
     </AppBar>
   );
 }
