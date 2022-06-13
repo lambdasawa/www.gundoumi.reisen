5275caf746782b6de08e3e3c6c98b2ebe1dafceb
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-06-13 20:29:13 +0900

    Fix build

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -11,28 +11,17 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-          />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="icon" href="/favicon.ico" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-          <Script
-            src="https://www.youtube.com/iframe_api"
-            strategy="beforeInteractive"
-            charSet="utf-8"
-          />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <Script src="https://www.youtube.com/iframe_api" strategy="beforeInteractive" charSet="utf-8" />
 
           {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
             <>
+              <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
               <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-              />
-              <Script
+                id="ga-script"
                 dangerouslySetInnerHTML={{
                   __html: `
                     window.dataLayer = window.dataLayer || [];
@@ -48,30 +37,18 @@ export default class MyDocument extends Document {
           <meta property="og:url" content="https://www.gundoumi.reisen/sei/" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="郡道美玲ファンサイト (非公式)" />
-          <meta
-            property="og:site_name"
-            content="郡道美玲ファンサイト (非公式)"
-          />
+          <meta property="og:site_name" content="郡道美玲ファンサイト (非公式)" />
           <meta
             property="og:description"
             content="このサイトはにじさんじ所属バーチャルライバー郡道美玲さんの非公式ファンサイトです。"
           />
-          <meta
-            property="og:image"
-            content="https://www.gundoumi.reisen/og.png"
-          />
+          <meta property="og:image" content="https://www.gundoumi.reisen/og.png" />
           <meta property="og:locale" content="ja_JP" />
 
           <meta name="twitter:card" content="summary_large_image"></meta>
           <meta name="twitter:site" content="@lambdasawa"></meta>
-          <meta
-            name="twitter:description"
-            content="郡道美玲ファンサイト (非公式)"
-          ></meta>
-          <meta
-            name="twitter:image:src"
-            content="https://www.gundoumi.reisen/og.png"
-          ></meta>
+          <meta name="twitter:description" content="郡道美玲ファンサイト (非公式)"></meta>
+          <meta name="twitter:image:src" content="https://www.gundoumi.reisen/og.png"></meta>
         </Head>
         <body>
           <Main />
@@ -121,9 +98,6 @@ MyDocument.getInitialProps = async (ctx) => {
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
   };
 };
